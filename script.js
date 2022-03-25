863a17815ba2c3988dcc2b6611571262a6e73c17
Luca <lucas.ribeiro711@gmail.com>
2022-03-25 14:49:21 -0300

    feat: add death muahahaha

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function verify() {
+function old() {
   let date = new Date();
   let year = date.getFullYear();
-  let inputAddYear = document.querySelector("#txtyear");
+  let inputYear = document.querySelector("#txtyear");
   let result = document.querySelector("#result");
   let img = document.createElement("img");
 
-  if (inputAddYear.value.length == 0 || inputAddYear.value > year) {
-    alert("ERRO, Verifique os dados e tente novamente!");
+  if (inputYear.value.length == 0 || inputYear.value > year) {
+    alert("ERRO | por favor verifique as informações e tente novamente");
   } else {
     let gender = document.getElementsByName("radsex");
-    let age = year - inputAddYear.value;
+    let age = year - inputYear.value;
     let prefix = "";
     let gen = "";
 
@@ -19,11 +19,14 @@ function verify() {
 
       if (age >= 0 && age < 10) {
         img.setAttribute("src", "assets/foto-bebe-m.png");
-      } else if (age <= 30) {
+      }
+      if (age > 10 && age < 18) {
         img.setAttribute("src", "assets/foto-jovem-m.png");
-      } else if (age <= 60) {
+      }
+      if (age > 18 && age < 30) {
         img.setAttribute("src", "assets/foto-adulto-m.png");
-      } else if (age > 60) {
+      }
+      if (age > 30 && age < 114) {
         img.setAttribute("src", "assets/foto-idoso-m.png");
       }
     } else if (gender[1].checked) {
@@ -32,17 +35,77 @@ function verify() {
 
       if (age >= 0 && age < 10) {
         img.setAttribute("src", "assets/foto-bebe-f.png");
-      } else if (age <= 30) {
+      }
+      if (age > 10 && age < 18) {
         img.setAttribute("src", "assets/foto-jovem-f.png");
-      } else if (age <= 60) {
+      }
+      if (age > 18 && age < 30) {
         img.setAttribute("src", "assets/foto-adulto-f.png");
-      } else if (age > 60) {
+      }
+      if (age > 30 && age < 114) {
         img.setAttribute("src", "assets/foto-idoso-f.png");
       }
     }
+    result.style.textAlign = "center";
+    result.innerHTML = `Encontramos ${prefix} ${gen} de ${age} anos`;
+    result.appendChild(img);
+  }
+}
 
-    result.style.textAlign = 'center'
-    result.innerHTML = `Encontramos ${prefix} ${gen} de ${age}`
-    result.appendChild(img)
+function verify() {
+  let date = new Date();
+  let year = date.getFullYear();
+  let input = document.querySelector("#txtyear");
+  let result = document.querySelector("#result");
+  let img = document.createElement("img");
+
+  if (input.value.length == 0 || input.value > year) {
+    alert("Erro, cheque os dados e tente novamente");
+  } else {
+    let gender = document.getElementsByName("radsex");
+    let age = year - input.value;
+    let gen = "";
+    let prefix = "";
+
+    if (gender[0].checked) {
+      prefix = "um";
+      gen = "homem";
+
+      if (age > 0 && age < 12) {
+        img.setAttribute("src", "assets/foto-bebe-m.png");
+      } else if (age >= 12 && age < 18) {
+        img.setAttribute("src", "assets/foto-jovem-m.png");
+      } else if (age >= 18 && age < 50) {
+        img.setAttribute("src", "assets/foto-adulto-m.png");
+      } else if (age >= 50 && age < 104) {
+        img.setAttribute("src", "assets/foto-idoso-m.png");
+      } else {
+        img.setAttribute("src", "assets/morte.jpeg");
+        gen = "cadaver?";
+      }
+    }
+
+    if (gender[1].checked) {
+      prefix = "uma";
+      gen = "mulher";
+
+      if (age > 0 && age < 12) {
+        img.setAttribute("src", "assets/foto-bebe-f.png");
+      } else if (age >= 12 && age < 18) {
+        img.setAttribute("src", "assets/foto-jovem-f.png");
+      } else if (age >= 18 && age < 50) {
+        img.setAttribute("src", "assets/foto-adulto-f.png");
+      } else if (age >= 50 && age < 104) {
+        img.setAttribute("src", "assets/foto-idoso-f.png");
+      } else {
+        img.setAttribute("src", "assets/morte.jpeg");
+        gen = "cadaver?";
+      }
+    }
+
+    result.style.textAlign = "center";
+    result.innerHTML = `Encontramos ${prefix} ${gen} de ${age} anos`;
+    result.appendChild(img);
+    img.style.padding = "25px";
   }
 }
